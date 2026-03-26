9bf6942c17ae8439334df79ab10ab860b917bf83
Aditinegi07 <aditinegi9675@gmail.com>
2026-03-26 21:55:57 +0530

    commit

--- UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.college.hackathon.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
